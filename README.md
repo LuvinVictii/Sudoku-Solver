7f2befaf473bf2eb1aeb2b4f38f114ea407b8525
jap <javakanaya@gmail.com>
2023-05-18 08:50:16 +0700

    update readme, renaming files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Sudoku-Solver
 Sudoku solver with simple UI for my Design and Analysis of Algorithms (DAA) course's quiz
+
+## Team Members: 
+1. Muhammad Daffa Harits - 5025211005
+2. Java Kanaya Prada - 5025211112
+3. Afiq Fawwaz Haidar - 5025211246
--- solver_with_GUI.py
+++ solver_with_GUI.py
@@ -81,7 +81,6 @@ def check_duplicate():
                     for j in range(start_col, start_col + 3):
                         if board[i][j] == num and (i != row or j != col):
                             return True
-
     return False
 
 
@@ -110,39 +109,41 @@ def solve_sudoku():
 def create_layout():
     root = tk.Tk()
     root.title("Sudoku Solver")
+    root.geometry("400x500")
+    root.configure(padx=10, pady=10,bg="white")
+    root.resizable(False, False)
 
     entry_list = []
     for i in range(9):
         row_entries = []
         for j in range(9):
             entry_var = StringVar()
-            entry = tk.Entry(root, width=2, justify='center', textvariable=entry_var)
-            entry.grid(row=i, column=j, padx=3, pady=3)
+            #entry = tk.Entry(root, width=3, font=("Arial", 14), justify='center', textvariable=entry_var)
             row_entries.append(entry_var)
         entry_list.append(row_entries)
 
-    # Add vertical grid lines
-    for j in range(1, 9):
-        if j % 3 == 0:
-            root.grid_columnconfigure(j, minsize=3)
-        else:
-            root.grid_columnconfigure(j, minsize=1)
+    solve_button = tk.Button(root, text="Solve Sudoku",  borderwidth=0, command=solve_sudoku, font=("Arial", 12, "bold"), fg="#D5E8FF", bg="#001B3C")
+    solve_button.grid(row=9, columnspan=12, pady=10)
 
-    # Add horizontal grid lines
-    for i in range(1, 9):
-        if i % 3 == 0:
-            root.grid_rowconfigure(i, minsize=3)
-        else:
-            root.grid_rowconfigure(i, minsize=1)
+    clear_button = tk.Button(root, text="Clear", borderwidth=0, command=clear_board, font=("Arial", 12,"bold"), fg="#D5E8FF", bg="#001B3C")
+    clear_button.grid(row=10, columnspan=12)
 
-    solve_button = tk.Button(root, text="Solve Sudoku", command=solve_sudoku)
-    solve_button.grid(row=9, columnspan=9, pady=10)
-
-    clear_button = tk.Button(root, text="Clear", command=clear_board)
-    clear_button.grid(row=10, columnspan=9)
-
-    status_label = tk.Label(root, text="Enter the Sudoku puzzle", fg="black")
+    status_label = tk.Label(root, text="Enter the Sudoku puzzle", fg="#001B3C", font=("Arial", 12,"bold"), bg="white")
     status_label.grid(row=11, columnspan=9, pady=10)
+
+    root.grid_columnconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8), weight=1, minsize=40)
+    root.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8), weight=1, minsize=40)
+
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            frame = tk.Frame(root, bg="#86B8F3")
+            frame.grid(row=i, column=j, rowspan=3, columnspan=3, padx=0, pady=0)
+            frame.grid_columnconfigure((0, 1, 2), weight=1, minsize=20)
+            frame.grid_rowconfigure((0, 1, 2), weight=1, minsize=20)
+            for x in range(3):
+                for y in range(3):
+                    entry = tk.Entry(frame, width=2, font=("Arial", 21), justify='center',bg="#00455A",fg="#86B8F3", textvariable=entry_list[i+x][j+y])
+                    entry.grid(row=x, column=y, padx=0.5, pady=0.5)
 
     return entry_list, status_label, root
 
